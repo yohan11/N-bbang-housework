36c67b80dff1c02bf7804c8e725ee90c529e5225
yohan11 <yipco77@naver.com>
2023-09-25 20:10:55 +0900

    회원가입, 메인페이지 생성

---
+++ src/pages/Main.js
@@ -0,0 +1,5 @@
+const Main = () => {
+  return <>메인페이지입니다</>;
+};
+
+export default Main;
---
+++ src/pages/SignUp.js
@@ -0,0 +1,7 @@
+import "./css/SignUp.css";
+
+const SignUp = () => {
+  return <div className="SignUp">회원가입 페이지 입니다.</div>;
+};
+
+export default SignUp;
