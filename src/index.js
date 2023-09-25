06f67259586369e584676053f53e6e7af4b6ad8d
yohan11 <yipco77@naver.com>
2023-09-25 20:10:21 +0900

    feat(#4): 카카오, 네이버 로그인으로 토큰 얻기

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import Main from "./pages/Main";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route exact path="KAKAO/login" element={<App />} />
+      <Route path="signup" element={<SignUp />}></Route>
+      <Route path="main" element={<Main />}></Route>
+    </Routes>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,6 +1,7 @@
-import ".//css/Intro.css";
+import "./css/Intro.css";
 import BottomButton from "../components/Buttons/BottomButton";
 import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Intro = () => {
   const [kakaoLoginUrl, setKakaoLoginUrl] = useState();
@@ -8,7 +9,15 @@ const Intro = () => {
   const [isKakaoButtonActive, setIsKakaoButtonActive] = useState(true);
   const [isNaverButtonActive, setIsNaverButtonActive] = useState(true);
 
-  // 페이지 로드 시 카카오, 네이버 로그인 URL을 받아옵니다
+  const [code, setCode] = useState();
+  const [state, setState] = useState();
+  const [loginPlatform, setLoginPlatform] = useState();
+  const [userRole, setUserRole] = useState();
+
+  const navigate = useNavigate();
+  const loginPath = useLocation();
+
+  // 페이지 로드 시 카카오, 네이버 로그인 URL을 받아옵니다.
   useEffect(() => {
     fetch("https://jubujob.com/auth/KAKAO/loginPage")
       .then((res) => {
@@ -52,8 +61,60 @@ const Intro = () => {
     window.location.href = naverLoginUrl;
   };
 
+  // 로그인 시 code와 state값을 추출합니다.
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const codeParam = urlParams.get("code");
+    const stateParam = urlParams.get("state");
+
+    if (codeParam && stateParam) {
+      if (loginPath.pathname.includes("KAKAO")) setLoginPlatform("KAKAO");
+      else if (loginPath.pathname.includes("NAVER")) setLoginPlatform("NAVER");
+      setCode(codeParam);
+      setState(stateParam);
+    }
+  }, []);
+
+  // code와 state값이 올바르게 저장되었을 경우 api를 호출하여 토큰을 받아와 세션 스토리지에 저장합니다.
+  useEffect(() => {
+    if (code && state) {
+      fetch(
+        `https://jubujob.com/auth/${loginPlatform}/login?code=${code}&state=${state}`,
+      )
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("API 요청 실패");
+          }
+          return res.json();
+        })
+        .then((json) => {
+          sessionStorage.setItem(
+            "accessToken",
+            JSON.stringify(json.resObj.accessToken),
+          );
+          sessionStorage.setItem(
+            "refreshToken",
+            JSON.stringify(json.resObj.refreshToken),
+          );
+          setUserRole(json.resObj.role);
+        })
+        .catch((error) => {
+          console.error("API 요청 중 오류 발생:", error);
+        });
+    }
+  }, [code, state]);
+
+  // 받아온 응답 값 내 user의 역할에 따라 다른 페이지로 이동합니다.
+  useEffect(() => {
+    if (userRole === "UNAUTH") {
+      navigate("/signup");
+    } else if (userRole === "AUTH") {
+      navigate("/main");
+    }
+  }, [userRole]);
+
   return (
-    <>
+    <div className="Intro">
       <div className="loginButtons">
         <BottomButton
           text="카카오로 로그인"
@@ -72,7 +133,7 @@ const Intro = () => {
           buttonActive={isNaverButtonActive}
         />
       </div>
-    </>
+    </div>
   );
 };
 
