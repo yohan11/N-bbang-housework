9ce9e6eb084ebe98a3b68288a8d9f72ef3240a10
yohan11 <yipco77@naver.com>
2023-10-17 11:46:07 +0900

    edit(#8): 모달 컴포넌트화

--- src/components/Boxes/TodoWorkBox.js
+++ src/components/Boxes/TodoWorkBox.js
@@ -1,8 +1,7 @@
 import "./css/TodoWorkBox.css";
 import Avatar from "../Others/Avatar";
-import Modal from "react-modal";
-import CategoryButton from "../Buttons/CategoryButton";
 import { useState } from "react";
+import MainModal from "../Others/MainModal";
 
 const TodoWorkBox = (props) => {
   const check_icon_style_default = {
@@ -43,123 +42,46 @@ const TodoWorkBox = (props) => {
           <Avatar userImage="/img/dummy_user.jpeg" />
         </div>
       </div>
-      <Modal
-        className="modal"
-        isOpen={activeMainModal}
-        onRequestClose={() => setActiveMainModal(false)}
-        style={{
-          overlay: {
-            position: "fixed",
-            height: "100%",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: "rgba(0, 0, 0, 0.3)",
-          },
-          content: {
-            position: "absolute",
-            top: "30%",
-            left: 0,
-            right: 0,
-            bottom: 0,
-            border: "none",
-            background: "#fff",
-            overflow: "auto",
-            WebkitOverflowScrolling: "touch",
-            borderRadius: "20px 20px 0 0",
-            outline: "none",
-            padding: 0,
-          },
-        }}
-      >
-        <div className="modal_contents">
-          <img src="/img/mission_photo.png" className="modal_img" />
-          <div className="modal_texts">
-            <div className="ftM boldTxt textCenter mt4">
-              {props.todoWorkName} 미션 클리어!
-            </div>
-            <div className="ftXSm thinTxt textCenter mt2">
-              사진을 찍어 단국대팟 하우스 멤버들에게
-              <br /> 미션 클리어 인증을 해보세요!
-            </div>
-            <div className="modal_buttons mt4">
-              <CategoryButton
-                borderRadius="10px"
-                categoryName="취소"
-                isSelected={false}
-                onClick={() => setActiveMainModal(false)}
-                width="40%"
-              ></CategoryButton>
-              <CategoryButton
-                borderRadius="10px"
-                categoryName="사진 인증하기"
-                isSelected={false}
-                onClick={() => {
-                  setWorkStatus("proceeding");
-                  setActiveMainModal(false);
-                }}
-                activeCamera={true}
-                width="40%"
-              ></CategoryButton>
 
-              <br />
-            </div>
+      <MainModal
+        activeModal={activeMainModal}
+        onRequestCloseFunc={() => setActiveMainModal(false)}
+        modalTop="30%"
+        titleText={props.todoWorkName + " 미션 클리어!"}
+        contentText={
+          <div>
+            사진을 찍어 단국대팟 하우스 멤버들에게
+            <br />
+            미션 클리어 인증을 해보세요!
           </div>
-        </div>
-      </Modal>
-      <Modal
-        className="modal"
-        isOpen={activeSubModal}
-        onRequestClose={() => setActiveSubModal(false)}
-        style={{
-          overlay: {
-            position: "fixed",
-            height: "100%",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: "rgba(0, 0, 0, 0.3)",
-          },
-          content: {
-            position: "absolute",
-            top: "25%",
-            left: 0,
-            right: 0,
-            bottom: 0,
-            border: "none",
-            background: "#fff",
-            overflow: "auto",
-            WebkitOverflowScrolling: "touch",
-            borderRadius: "20px 20px 0 0",
-            outline: "none",
-            padding: 0,
-          },
+        }
+        twoBtn={true}
+        firstBtnText="취소"
+        firstBtnOnClick={() => setActiveMainModal(false)}
+        secondBtnText="사진 인증하기"
+        secondBtnOnClick={() => {
+          setWorkStatus("proceeding");
+          setActiveMainModal(false);
         }}
-      >
-        <div className="modal_contents">
-          <img src="/img/mission_check.gif" className="modal_sub_img" />
-          <div className="ftM boldTxt textCenter mt3">
-            멤버들의 인증을 기다려주세요!
-          </div>
-          <div className="ftXSm thinTxt textCenter mt2">
+        activeCamera={true}
+      />
+      <MainModal
+        activeModal={activeSubModal}
+        onRequestCloseFunc={() => setActiveSubModal(false)}
+        modalTop="25%"
+        titleText="멤버들의 인증을 기다려주세요!"
+        contentText={
+          <div>
             단국대팟 하우스 멤버들 중 한명 이상이 사진을
-            <br /> 보고 수행 완료 버튼을 눌러야 미션이 완료돼요.
+            <br />
+            보고 수행 완료 버튼을 눌러야 미션이 완료돼요.
           </div>
-          <div className="modal_buttons mt4">
-            <CategoryButton
-              borderRadius="10px"
-              categoryName="이전 페이지로 돌아가기"
-              isSelected={false}
-              onClick={() => {
-                setActiveSubModal(false);
-              }}
-              width="80%"
-            ></CategoryButton>
-          </div>
-        </div>
-      </Modal>
+        }
+        twoBtn={false}
+        BtnText="이전 페이지로 돌아가기"
+        BtnOnClick={() => setActiveSubModal(false)}
+        activeCamera={false}
+      />
     </>
   );
 };
---
+++ src/components/Others/MainModal.js
@@ -0,0 +1,78 @@
+import Modal from "react-modal";
+import CategoryButton from "../Buttons/CategoryButton";
+
+const MainModal = (props) => {
+  return (
+    <Modal
+      className="modal"
+      isOpen={props.activeModal}
+      onRequestClose={props.onRequestCloseFunc}
+      style={{
+        overlay: {
+          position: "fixed",
+          height: "100%",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: "rgba(0, 0, 0, 0.3)",
+        },
+        content: {
+          position: "absolute",
+          top: props.modalTop,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          border: "none",
+          background: "#fff",
+          overflow: "auto",
+          WebkitOverflowScrolling: "touch",
+          borderRadius: "20px 20px 0 0",
+          outline: "none",
+          padding: 0,
+        },
+      }}
+    >
+      <div className="modal_contents">
+        <img src="/img/mission_photo.png" className="modal_img" />
+        <div className="modal_texts">
+          <div className="ftM boldTxt textCenter mt4">{props.titleText}</div>
+          <div className="ftXSm thinTxt textCenter mt2">
+            {props.contentText}
+          </div>
+          <div className="modal_buttons mt4">
+            {props.twoBtn === true ? (
+              <>
+                <CategoryButton
+                  borderRadius="10px"
+                  categoryName={props.firstBtnText}
+                  isSelected={false}
+                  onClick={props.firstBtnOnClick}
+                  width="40%"
+                ></CategoryButton>
+                <CategoryButton
+                  borderRadius="10px"
+                  categoryName={props.secondBtnText}
+                  isSelected={false}
+                  onClick={props.secondBtnOnClick}
+                  activeCamera={props.activeCamera}
+                  width="40%"
+                ></CategoryButton>
+              </>
+            ) : (
+              <CategoryButton
+                borderRadius="10px"
+                categoryName={props.BtnText}
+                isSelected={false}
+                onClick={props.BtnOnClick}
+                activeCamera={props.activeCamera}
+                width="80%"
+              ></CategoryButton>
+            )}
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+export default MainModal;
